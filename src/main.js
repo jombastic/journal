a71b38d7693d3cd0443916a156ffbc9c306d391d
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-06-02 10:21:39 +0200

    Modify addEntry function to take 3 parameters for entry object, numberOfLetters and teaser and append a panel with title, body and footer in html div entries

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import { Entry } from "./entry.js";
 import { getLetterCount, getTeaser } from "./vowels-and-consonants.js";
 
+function addEntry(entry, numOfLetters, teaser) {
+  var numberOfVowels = numOfLetters[0];
+  var numberOfConsonants = numOfLetters[1];
+  $("#entries").append('<div class="panel panel-default">' +
+                          '<div class="panel-heading">' +
+                            '<h3 class="panel-title">' + entry.title +
+                            ': ' + teaser + '</h3>' +
+                          '</div>' +
+                          '<div class="panel-body">'
+                            + entry.text +
+                          '</div>' +
+                          '<div class="panel-footer">' +
+                          '<p>Number of words: ' + entry.numberOfWords() + '</p>' +
+                          '<p>Number of vowels: ' + numberOfVowels + '</p>' +
+                          '<p>Number of consonants: ' + numberOfConsonants + '</p>' +
+                          '</div>' +
+                        '</div>');
+}
+
 $(function() {
   $("form#journal").submit(function(event) {
     event.preventDefault();
@@ -8,15 +27,10 @@ $(function() {
     var title = $("input#journal-title").val();
     var body = $("textarea#journal-body").val();
     var journalEntry = new Entry(title, body);
-    var numberOfWords = journalEntry.numberOfWords();
-    var numOfletters = getLetterCount(journalEntry.text);
-    var numberOfVowels = numOfletters[0];
-    var numberOfConsonants = numOfletters[1];
+
+    var numOfLetters = getLetterCount(journalEntry.text);
     var teaser = getTeaser(journalEntry.text);
 
-    $(".number-of-words").text(numberOfWords);
-    $(".number-of-vowels").text(numberOfVowels);
-    $(".number-of-consonants").text(numberOfConsonants);
-    $(".teaser").text(teaser);
+    addEntry(journalEntry, numOfLetters, teaser);
   });
 });
